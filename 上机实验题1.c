f6a1c5199079871a35a109f5e3b92e39879bb412
zaixiaxiaozhao <59325658+zaixiaxiaozhao@users.noreply.github.com>
2020-04-19 13:05:19 +0800

    Create 上机实验题1.c
    上机实验题1：
    
    设计程序，输出所有小于等于n（n为一个大于2的正整数，比如100）的素数，要求：
    
    （1）每行输出10个素数；（2）尽可能采用较优算法。

---
+++ 上机实验题1.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<math.h>
+#include<time.h>
+clock_t start, stop;
+int main()
+{
+	start = clock();
+	int m, n, i, k, num = 1;
+	double C;
+	printf("输入所求数的值：\n");
+	scanf_s("%d", &m);
+	for (n = 3; n <= m; n += 2)
+	{
+		k = sqrt(n);       //开方省步骤
+		for (i = 2; i <= k; i++)
+			if (n % i == 0)break;
+		if (i == k + 1)
+		{
+			printf("%d ", n);
+			num++;
+			if (num % 10 == 0)
+				printf("\n");
+		}
+	}
+	stop = clock();
+	C = ((double)(stop - start)) / CLK_TCK;
+	printf("%f", C);
+	printf("\n统计结果：小于等于n的素数有%d个。\n", num);
+
+	return 0;
+}
