a013a4ba4581e4a34b10e9c47422e25ba2ccd528
zaixiaxiaozhao <59325658+zaixiaxiaozhao@users.noreply.github.com>
2020-06-08 17:08:45 +0800

    Update 上机实验题2.c
    建立顺序表存储数据序列（10，20，30，40，50，60，70，80，90,100），要求：
    
    （1）输出顺序表中的所有元素；
    
    （2）输出顺序表中的最大值和最小值；
    
    （3）键盘输入一个数x，如x在表中返回其在表中的位序，不在返回相应提示信息。
    
    （4）删除顺序表中的第8个元素，并输出顺序表中的所有元素；
    
    （5）在第5个元素后面插入新元素55，并输出顺序表中的所有元素；
    
    （6）输出顺序表的长度。

--- 上机实验题2.c
+++ 上机实验题2.c
@@ -1,20 +1,20 @@
 #include<stdio.h>
-#define Size 20
+#define Size 10
 typedef struct Table                            //自定义顺序表
 {
 	int* head;
 	int length;
-	int size;
+	int size;	
 }table;
 
 table initTable()                               //初始化表
 {
 	table t;
 	t.head = (int*)malloc(Size * sizeof(int));
-	if (!t.head)
+	if (!t.head) 
 	{
 		printf("初始化失败");
-		exit(0);
+			exit(0);
 	}
 	t.length = 0;
 	t.size = Size;
@@ -27,18 +27,18 @@ void displayTable(table t)						//输出顺序表中元素
 	{
 		printf("%d ", t.head[i]);
 	}
-	printf("/n");
+	printf("\n");
 }
 
 void Maximum(table t)							//输出顺序表中的最大值
 {
 	int max = t.head[0];
-	for (int i = 1; i < t.length; i++)
+	for (int i = 1; i < t.length; i ++)
 	{
 		if (t.head[i] > max)
 			max = t.head[i];
 	}
-	printf("顺序表中的最大值为%d/n", max);
+	printf("顺序表中的最大值为%d", max);
 }
 
 void Minimum(table t)							//输出顺序表中的最小值
@@ -49,23 +49,23 @@ void Minimum(table t)							//输出顺序表中的最小值
 		if (t.head[i] < min)
 			min = t.head[i];
 	}
-	printf("顺序表中的最小值为%d/n", min);
+	printf("顺序表中的最小值为%d\n", min);
 }
 
-int LocateElem(table t, int elem)				//查找数值返回信息
+void LocateElem(table t, int elem)				//查找数值返回信息
 {
 	for (int i = 0; i < t.length; i++)
 	{
 		if (t.head[i] == elem)
-			return("/n该数值在表中的第%d位/n", i);
+			return(printf("\n该数值在表中的第%d位\n", i));
 	}
-	return("/n该数值不在表中/n");
+	return(printf("\n该数值不在表中\n"));
 }
 
-table delTable(table t, int add)					//删除元素
+table delTable(table t,int add)					//删除元素
 {
 	if (add > t.length || add < 1) {
-		printf("被删除元素的位置有误/n");
+		printf("被删除元素的位置有误\n");
 		exit(0);
 	}
 	for (int i = add; i < t.length; i++)
@@ -109,31 +109,31 @@ int main()
 	//（1）输出顺序表中的所有元素；
 	printf("原顺序表：");
 	displayTable(t);
-	printf("/n");
+	printf("\n");
 
 	//（2）输出顺序表中的最大值和最小值；
 	printf("顺序表中的最大值和最小值");
 	Maximum(t);
 	Minimum(t);
-	printf("/n");
+	printf("\n");
 
 	//（3）键盘输入一个数x，如x在表中返回其在表中的位序，不在返回相应提示信息。
-	printf("查找元素位置/n");
+	printf("查找元素位置\n");
 	printf("请输入您要查找的数：");
 	scanf_s("%d", &elem);
 	LocateElem(t, elem);
 
 	//（4）删除顺序表中的第8个元素，并输出顺序表中的所有元素；
-	printf("删除第8个元素/n");
+	printf("删除第8个元素\n");
 	delTable(t, 8);
 	displayTable(t);
 
 	//（5）在第5个元素后面插入新元素55，并输出顺序表中的所有元素；
-	printf("在第5个元素后面插入新元素55/n");
+	printf("在第5个元素后面插入新元素55\n");
 	addTable(t, 55, 5);
 	displayTable(t);
 
 	//（6）输出顺序表的长度。* /
-	printf("顺序表的长度为%d/n：", t.length);
+	printf("顺序表的长度为%d\n：", t.length);
 	return 0;
 }
